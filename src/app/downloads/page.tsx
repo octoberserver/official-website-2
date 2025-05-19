05118a40ad941d07e0326774e03fd8a4096eb9ee
october <octoberserver@gmail.com>
2025-05-19 15:49:44 +0800

    : )

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,6 +1,8 @@
 import Downloads from "october-site/page-components/Downloads"
 import { Modpack } from "october-site/util/types"
 
+export const dynamic = "force-dynamic"
+
 export default async function DownloadsPage() {
     const res = await fetch("https://api.modpack-manager.octsrv.org/servers/main/modpack")
     const data: Modpack = await res.json()
